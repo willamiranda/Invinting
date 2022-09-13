dd06729a9f6066fabc36d769aa20272b1fb2e688
Wiilames <willmiranda.94@gmail.com>
2022-09-12 22:08:19 -0400

    update and delete from table implemented

--- app/src/main/java/com/willmiranda/inviting/repository/GuestRepository.kt
+++ app/src/main/java/com/willmiranda/inviting/repository/GuestRepository.kt
@@ -33,13 +33,48 @@ class GuestRepository private constructor(context: Context) {
             values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
 
             db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, values)
-             true
+            true
         } catch (e: Exception) {
-             false
+            false
         }
     }
 
-    fun update() {
+    fun update(guest: GuestModel): Boolean {
+
+
+        return try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+    }
+    fun delete(id: Int): Boolean {
+
+
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection,args)
+            true
+        } catch (e: Exception) {
+            false
+        }
 
     }
 }
